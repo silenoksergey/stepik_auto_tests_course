37795fad94e5ad8397c856bcad63c2be3215ad5c
Sergey Silenok <silenoksergey34@gmail.com>
2023-03-29 00:21:48 +0500

    refactor

--- test_conftest.py
+++ test_conftest.py
@@ -9,8 +9,8 @@ def test_find_correct_number(browser, test_links):
     link = f"https://stepik.org/lesson/{test_links}/step/1"
     browser.get(link)
     browser.find_element(By.ID, "ember33").click()
-    browser.find_element(By.ID, "id_login_email").send_keys("silenoksergey34@gmail.com")
-    browser.find_element(By.ID, "id_login_password").send_keys("19971222")
+    browser.find_element(By.ID, "id_login_email").send_keys("login")
+    browser.find_element(By.ID, "id_login_password").send_keys("password")
     browser.find_element(By.CLASS_NAME, "sign-form__btn.button_with-loader").click()
     time.sleep(5)
     WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, "ember-text-area.ember-view.textarea.string-quiz__textarea"))).send_keys(math.log(int(time.time())))
